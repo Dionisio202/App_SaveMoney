055f9a899a15031a3cdf43cfc9bce200315ff4bc
Edison <solisedison@outlook.com>
2023-10-25 17:50:20 -0500

    create class Pre-Splash

--- lib/PreSpalsh.dart
+++ lib/PreSpalsh.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class PreSplash extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<PreSplash> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Aquí puedes construir la interfaz de usuario de tu pantalla de presentación.
+    return const Scaffold(
+      body: Center(
+        child: Text('Splash Screen'),
+      ),
+    );
+  }
+}
