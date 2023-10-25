29817c7db7360e20d742a07b71201c23ac4e4876
Edison <solisedison@outlook.com>
2023-10-25 18:26:24 -0500

    created animate line

--- lib/PreSpalsh.dart
+++ lib/PreSpalsh.dart
@@ -2,21 +2,58 @@ import 'package:flutter/material.dart';
 
 class PreSplash extends StatefulWidget {
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  _PreSplashScreenState createState() => _PreSplashScreenState();
 }
 
-class _SplashScreenState extends State<PreSplash> {
+class _PreSplashScreenState extends State<PreSplash>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 600),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: 0.7).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // Aquí puedes construir la interfaz de usuario de tu pantalla de presentación.
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Color(0xFFE7E7D9),
       body: Center(
-        child: Text('Splash Screen'),
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    height: 4,
+                    width: _animation.value * MediaQuery.of(context).size.width,
+                    color: Color.fromARGB(255, 110, 109, 33),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
