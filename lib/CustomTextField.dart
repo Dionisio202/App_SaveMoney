fe8d89ebc35d1c1343109bd2b08d18593e6f2b52
Edison <solisedison@outlook.com>
2023-10-27 19:40:13 -0500

    Created the password textfield

---
+++ lib/CustomTextField.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String labelText;
+  final double fontSize;
+  final double textFieldWidth;
+  final IconData icon;
+  final ValueChanged<String>? onChanged;
+
+  CustomTextField({
+    required this.labelText,
+    required this.fontSize,
+    required this.textFieldWidth,
+    required this.icon,
+    this.onChanged,
+  });
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.textFieldWidth,
+      child: TextField(
+        onChanged: widget.onChanged,
+        style: TextStyle(fontSize: widget.fontSize),
+        obscureText: _obscureText,
+        decoration: InputDecoration(
+          labelText: widget.labelText,
+          labelStyle: TextStyle(fontSize: widget.fontSize),
+          contentPadding: EdgeInsets.all(8.0),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black, width: 1.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue, width: 2.0),
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(widget.icon),
+            onPressed: () {
+              setState(() {
+                _obscureText = !_obscureText;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Login.dart
+++ lib/Login.dart
@@ -1,3 +1,4 @@
+import 'package:app/CustomTextField.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -9,13 +10,6 @@ class _LoginScreenState extends State<Login> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
-  void _handleLogin() {
-    // Aquí puedes agregar la lógica para autenticar al usuario con el correo electrónico y la contraseña ingresados.
-    // Por ejemplo, puedes usar Firebase, un servidor API, o cualquier otro método de autenticación.
-
-    // Después de la autenticación, puedes navegar a la pantalla principal o mostrar un mensaje de error en caso de problemas.
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,23 +20,26 @@ class _LoginScreenState extends State<Login> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Correo Electrónico',
-              ),
+            CustomTextField(
+              labelText: 'Contraseña',
+              fontSize: 16.0,
+              textFieldWidth: 300.0, // Especifica el ancho deseado.
+              icon: Icons.visibility,
+              onChanged: (value) {
+                // Tu lógica de manejo del cambio.
+              },
             ),
             TextFormField(
               controller: _passwordController,
               decoration: InputDecoration(
                 labelText: 'Contraseña',
               ),
-              obscureText: true,
+              obscureText: false,
             ),
-            ElevatedButton(
-              onPressed: _handleLogin,
+            /* ElevatedButton(
+              onPressed: ,
               child: Text('Iniciar Sesión'),
-            ),
+            ),*/
           ],
         ),
       ),
